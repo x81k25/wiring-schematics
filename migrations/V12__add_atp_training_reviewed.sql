1139bec11f905dc6570dd3bcf36c1edd6cc9d135
x81k25 <x81k25@gmail.com>
2025-07-02 10:55:37 -0500

    add atp.training.reviewed

--- migrations/V12__add_atp_training_reviewed.sql
+++
@@ -1,5 +0,0 @@
--- Add 'reviewed' column if it doesn't exist
-ALTER TABLE atp.training ADD COLUMN reviewed BOOLEAN DEFAULT FALSE;
-
--- add comment on column
-COMMENT ON COLUMN atp.training.reviewed IS 'denotes whether or not element had been reviewed; determines inclusion into training data set';

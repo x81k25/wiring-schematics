13c5f8f3be9ba2cf74e2ae751dc2f39c2e2e7589
x81k25 <x81k25@gmail.com>
2025-08-10 20:57:07 -0500

    removed duplicates

--- wiring-schematics/migrations/V21__grant_rear_diff_atp_prediction_select.sql
+++
@@ -1,5 +0,0 @@
--- Grant SELECT permission on atp.prediction table to rear_diff user
--- This allows rear_diff to read the atp.prediction table
-
-GRANT USAGE ON SCHEMA atp TO rear_diff;
-GRANT SELECT ON atp.prediction TO rear_diff;
